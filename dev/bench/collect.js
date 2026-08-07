5864fba69a5e5f764a3d3591682cfff6decc6d5c
Rudolf Polzer <divVerent@gmail.com>
2026-08-07 06:27:30 +0200

    Also factor out the CSS, and add a page collecting both GCC and Clang data.
    Change-Id: I18721cf0eda9a661f08438bdc358badbe0414208

---
+++ dev/bench/collect.js
@@ -0,0 +1,3 @@
+window.BENCHMARK_DATA_ENTRIES ||= {};
+window.BENCHMARK_DATA_ENTRIES = Object.assign(window.BENCHMARK_DATAS || {}, window.BENCHMARK_DATA.entries);
+window.BENCHMARK_DATA.entries = window.BENCHMARK_DATA_ENTRIES;
