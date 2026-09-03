dbe57ba5fd8fe2d4697c452336cfa661ddbecf2b
Rudolf Polzer <divVerent@gmail.com>
2026-09-03 11:36:14 +0200

    Add data directories for LTO.
    Change-Id: I7705e0ffd0b409fd13d960fb93a1b61d05c2ffdc

---
+++ dev/bench/x86_64-linux-clang-lto/data.js
@@ -0,0 +1,7 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1788427845339,
+  "repoUrl": "https://github.com/divVerent/boringssl",
+  "entries": {
+    "Benchmark on x8-64 Linux Clang with LTO": []
+  }
+}
---
+++ dev/bench/x86_64-linux-gcc-lto/data.js
@@ -0,0 +1,7 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1788427880929,
+  "repoUrl": "https://github.com/divVerent/boringssl",
+  "entries": {
+    "Benchmark on x8-64 Linux GCC with LTO": []
+  }
+}
